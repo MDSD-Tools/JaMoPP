b7ca0a515d1043b6eb2557d49854efff6c8d8078
unknown <yannickkraml@gmail.com>
2023-11-22 18:26:02 +0100

    Fixed imports in AnnotationInstanceOrModifierConverterUtility.java

--- bundles/jamopp.parser.jdt/src/jamopp/parser/jdt/AnnotationInstanceOrModifierConverterUtility.java
+++ bundles/jamopp.parser.jdt/src/jamopp/parser/jdt/AnnotationInstanceOrModifierConverterUtility.java
@@ -23,76 +23,86 @@ import org.eclipse.jdt.core.dom.MemberValuePair;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.NormalAnnotation;
 import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
+import org.emftext.language.java.annotations.AnnotationAttributeSetting;
+import org.emftext.language.java.annotations.AnnotationInstance;
+import org.emftext.language.java.annotations.AnnotationParameterList;
+import org.emftext.language.java.annotations.AnnotationValue;
+import org.emftext.language.java.annotations.AnnotationsFactory;
+import org.emftext.language.java.annotations.SingleAnnotationParameter;
+import org.emftext.language.java.arrays.ArraysFactory;
+import org.emftext.language.java.expressions.AssignmentExpressionChild;
+import org.emftext.language.java.members.InterfaceMethod;
+import org.emftext.language.java.modifiers.AnnotationInstanceOrModifier;
+import org.emftext.language.java.modifiers.ModifiersFactory;
 
 class AnnotationInstanceOrModifierConverterUtility {
-	static org.emftext.language.java.modifiers.AnnotationInstanceOrModifier converToModifierOrAnnotationInstance(IExtendedModifier mod) {
+
+	static AnnotationInstanceOrModifier converToModifierOrAnnotationInstance(IExtendedModifier mod) {
 		if (mod.isModifier()) {
 			return convertToModifier((Modifier) mod);
 		}
 		return convertToAnnotationInstance((Annotation) mod);
 	}
-	
+
 	static org.emftext.language.java.modifiers.Modifier convertToModifier(Modifier mod) {
 		org.emftext.language.java.modifiers.Modifier result = null;
 		if (mod.isAbstract()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createAbstract();
+			result = ModifiersFactory.eINSTANCE.createAbstract();
 		} else if (mod.isDefault()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createDefault();
+			result = ModifiersFactory.eINSTANCE.createDefault();
 		} else if (mod.isFinal()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createFinal();
+			result = ModifiersFactory.eINSTANCE.createFinal();
 		} else if (mod.isNative()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createNative();
+			result = ModifiersFactory.eINSTANCE.createNative();
 		} else if (mod.isPrivate()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createPrivate();
+			result = ModifiersFactory.eINSTANCE.createPrivate();
 		} else if (mod.isProtected()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createProtected();
+			result = ModifiersFactory.eINSTANCE.createProtected();
 		} else if (mod.isPublic()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createPublic();
+			result = ModifiersFactory.eINSTANCE.createPublic();
 		} else if (mod.isStatic()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createStatic();
+			result = ModifiersFactory.eINSTANCE.createStatic();
 		} else if (mod.isStrictfp()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createStrictfp();
+			result = ModifiersFactory.eINSTANCE.createStrictfp();
 		} else if (mod.isSynchronized()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createSynchronized();
+			result = ModifiersFactory.eINSTANCE.createSynchronized();
 		} else if (mod.isTransient()) {
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createTransient();
+			result = ModifiersFactory.eINSTANCE.createTransient();
 		} else { // mod.isVolatile()
-			result = org.emftext.language.java.modifiers.ModifiersFactory.eINSTANCE.createVolatile();
+			result = ModifiersFactory.eINSTANCE.createVolatile();
 		}
 		LayoutInformationConverter.convertToMinimalLayoutInformation(result, mod);
 		return result;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	static org.emftext.language.java.annotations.AnnotationInstance convertToAnnotationInstance(Annotation annot) {
-		org.emftext.language.java.annotations.AnnotationInstance result = org.emftext.language.java.annotations.AnnotationsFactory.eINSTANCE.createAnnotationInstance();
+	static AnnotationInstance convertToAnnotationInstance(Annotation annot) {
+		AnnotationInstance result = AnnotationsFactory.eINSTANCE.createAnnotationInstance();
 		BaseConverterUtility.convertToNamespacesAndSet(annot.getTypeName(), result);
 		org.emftext.language.java.classifiers.Annotation proxyClass;
 		IAnnotationBinding binding = annot.resolveAnnotationBinding();
 		if (binding == null) {
 			proxyClass = JDTResolverUtility.getAnnotation(annot.getTypeName().getFullyQualifiedName());
 		} else {
-			proxyClass = JDTResolverUtility.getAnnotation(binding.getAnnotationType()); 
+			proxyClass = JDTResolverUtility.getAnnotation(binding.getAnnotationType());
 		}
 		result.setAnnotation(proxyClass);
 		if (annot.isSingleMemberAnnotation()) {
-			org.emftext.language.java.annotations.SingleAnnotationParameter param = org.emftext.language.java.annotations.AnnotationsFactory.eINSTANCE
-				.createSingleAnnotationParameter();
+			SingleAnnotationParameter param = AnnotationsFactory.eINSTANCE.createSingleAnnotationParameter();
 			result.setParameter(param);
 			SingleMemberAnnotation singleAnnot = (SingleMemberAnnotation) annot;
 			TypeInstructionSeparationUtility.addSingleAnnotationParameter(singleAnnot.getValue(), param);
 		} else if (annot.isNormalAnnotation()) {
-			org.emftext.language.java.annotations.AnnotationParameterList param = org.emftext.language.java.annotations.AnnotationsFactory.eINSTANCE
-				.createAnnotationParameterList();
+			AnnotationParameterList param = AnnotationsFactory.eINSTANCE.createAnnotationParameterList();
 			result.setParameter(param);
 			NormalAnnotation normalAnnot = (NormalAnnotation) annot;
 			normalAnnot.values().forEach(obj -> {
 				MemberValuePair memVal = (MemberValuePair) obj;
-				org.emftext.language.java.annotations.AnnotationAttributeSetting attrSet = org.emftext.language.java.annotations.AnnotationsFactory.eINSTANCE
-					.createAnnotationAttributeSetting();
-				org.emftext.language.java.members.InterfaceMethod methodProxy;
+				AnnotationAttributeSetting attrSet = AnnotationsFactory.eINSTANCE.createAnnotationAttributeSetting();
+				InterfaceMethod methodProxy;
 				if (memVal.resolveMemberValuePairBinding() != null) {
-					 methodProxy = JDTResolverUtility.getInterfaceMethod(memVal.resolveMemberValuePairBinding().getMethodBinding());
+					methodProxy = JDTResolverUtility
+							.getInterfaceMethod(memVal.resolveMemberValuePairBinding().getMethodBinding());
 				} else {
 					methodProxy = JDTResolverUtility.getInterfaceMethod(memVal.getName().getIdentifier());
 					if (!proxyClass.getMembers().contains(methodProxy)) {
@@ -109,21 +119,20 @@ class AnnotationInstanceOrModifierConverterUtility {
 		LayoutInformationConverter.convertToMinimalLayoutInformation(result, annot);
 		return result;
 	}
-	
-	static org.emftext.language.java.annotations.AnnotationValue convertToAnnotationValue(Expression expr) {
+
+	static AnnotationValue convertToAnnotationValue(Expression expr) {
 		if (expr instanceof Annotation) {
 			return convertToAnnotationInstance((Annotation) expr);
 		}
 		if (expr.getNodeType() == ASTNode.ARRAY_INITIALIZER) {
 			return convertToArrayInitializer((ArrayInitializer) expr);
 		}
-		return (org.emftext.language.java.expressions.AssignmentExpressionChild)
-			ExpressionConverterUtility.convertToExpression(expr);
+		return (AssignmentExpressionChild) ExpressionConverterUtility.convertToExpression(expr);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	static org.emftext.language.java.arrays.ArrayInitializer convertToArrayInitializer(ArrayInitializer arr) {
-		org.emftext.language.java.arrays.ArrayInitializer result = org.emftext.language.java.arrays.ArraysFactory.eINSTANCE.createArrayInitializer();
+		org.emftext.language.java.arrays.ArrayInitializer result = ArraysFactory.eINSTANCE.createArrayInitializer();
 		arr.expressions().forEach(obj -> {
 			org.emftext.language.java.arrays.ArrayInitializationValue value = null;
 			Expression expr = (Expression) obj;
