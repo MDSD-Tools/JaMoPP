78830c7b862ab881133af452130578793f20570b
unknown <yannickkraml@gmail.com>
2024-01-05 00:16:27 +0100

    Revert "Refactored LambdaParametersPrinterImpl"
    This reverts commit 5764c4cae2b8a9652600ef4e5819f8b55afdd399.

--- bundles/tools.mdsd.jamopp.printer/src/tools/mdsd/jamopp/printer/implementation/LambdaParametersPrinterImpl.java
+++ bundles/tools.mdsd.jamopp.printer/src/tools/mdsd/jamopp/printer/implementation/LambdaParametersPrinterImpl.java
@@ -3,13 +3,14 @@ package tools.mdsd.jamopp.printer.implementation;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
-import javax.inject.Inject;
-
 import tools.mdsd.jamopp.model.java.expressions.ImplicitlyTypedLambdaParameters;
 import tools.mdsd.jamopp.model.java.expressions.LambdaParameters;
 import tools.mdsd.jamopp.model.java.expressions.SingleImplicitLambdaParameter;
 import tools.mdsd.jamopp.model.java.parameters.OrdinaryParameter;
 import tools.mdsd.jamopp.model.java.parameters.VariableLengthParameter;
+
+import javax.inject.Inject;
+
 import tools.mdsd.jamopp.printer.interfaces.Printer;
 
 public class LambdaParametersPrinterImpl implements Printer<LambdaParameters> {
@@ -27,43 +28,31 @@ public class LambdaParametersPrinterImpl implements Printer<LambdaParameters> {
 	@Override
 	public void print(LambdaParameters element, BufferedWriter writer) throws IOException {
 		if (element instanceof SingleImplicitLambdaParameter) {
-			handleSingleImplicitLambdaParameter(element, writer);
-		} else if (element instanceof ImplicitlyTypedLambdaParameters) {
-			handleImplicitlyTypedLambdaParameters(element, writer);
+			writer.append(element.getParameters().get(0).getName());
 		} else {
-			handleOther(element, writer);
-		}
-		writer.append(")");
-	}
-
-	private void handleOther(LambdaParameters element, BufferedWriter writer) throws IOException {
-		writer.append("(");
-		for (var index = 0; index < element.getParameters().size(); index++) {
-			var param = element.getParameters().get(index);
-			if (param instanceof OrdinaryParameter) {
-				ordinaryParameterPrinter.print((OrdinaryParameter) param, writer);
+			if (element instanceof ImplicitlyTypedLambdaParameters) {
+				writer.append("(");
+				for (var index = 0; index < element.getParameters().size(); index++) {
+					writer.append(element.getParameters().get(index).getName());
+					if (index < element.getParameters().size() - 1) {
+						writer.append(", ");
+					}
+				}
 			} else {
-				variableLengthParameterPrinter.print((VariableLengthParameter) param, writer);
+				writer.append("(");
+				for (var index = 0; index < element.getParameters().size(); index++) {
+					var param = element.getParameters().get(index);
+					if (param instanceof OrdinaryParameter) {
+						this.ordinaryParameterPrinter.print((OrdinaryParameter) param, writer);
+					} else {
+						this.variableLengthParameterPrinter.print((VariableLengthParameter) param, writer);
+					}
+					if (index < element.getParameters().size() - 1) {
+						writer.append(", ");
+					}
+				}
 			}
-			if (index < element.getParameters().size() - 1) {
-				writer.append(", ");
-			}
-		}
-	}
-
-	private void handleSingleImplicitLambdaParameter(LambdaParameters element, BufferedWriter writer)
-			throws IOException {
-		writer.append(element.getParameters().get(0).getName());
-	}
-
-	private void handleImplicitlyTypedLambdaParameters(LambdaParameters element, BufferedWriter writer)
-			throws IOException {
-		writer.append("(");
-		for (var index = 0; index < element.getParameters().size(); index++) {
-			writer.append(element.getParameters().get(index).getName());
-			if (index < element.getParameters().size() - 1) {
-				writer.append(", ");
-			}
+			writer.append(")");
 		}
 	}
 
