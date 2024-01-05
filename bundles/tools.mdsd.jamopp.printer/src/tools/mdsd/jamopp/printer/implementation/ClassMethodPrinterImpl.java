bc18c86d0dbd1a024166e623d20672e26024d9d5
unknown <yannickkraml@gmail.com>
2024-01-05 13:52:57 +0100

    Refactored ClassMethodPrinterImpl

--- bundles/tools.mdsd.jamopp.printer/src/tools/mdsd/jamopp/printer/implementation/ClassMethodPrinterImpl.java
+++ bundles/tools.mdsd.jamopp.printer/src/tools/mdsd/jamopp/printer/implementation/ClassMethodPrinterImpl.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import tools.mdsd.jamopp.model.java.arrays.ArrayDimension;
 import tools.mdsd.jamopp.model.java.classifiers.Enumeration;
 import tools.mdsd.jamopp.model.java.generics.TypeParametrizable;
@@ -16,9 +18,6 @@ import tools.mdsd.jamopp.model.java.modifiers.Static;
 import tools.mdsd.jamopp.model.java.parameters.Parametrizable;
 import tools.mdsd.jamopp.model.java.statements.Statement;
 import tools.mdsd.jamopp.model.java.types.TypeReference;
-
-import javax.inject.Inject;
-
 import tools.mdsd.jamopp.printer.interfaces.Printer;
 
 public class ClassMethodPrinterImpl implements Printer<ClassMethod> {
@@ -47,6 +46,24 @@ public class ClassMethodPrinterImpl implements Printer<ClassMethod> {
 
 	@Override
 	public void print(ClassMethod element, BufferedWriter writer) throws IOException {
+		if (shouldNotBePrinted(element)) {
+			return;
+		}
+		annotableAndModifiablePrinter.print(element, writer);
+		typeParametrizablePrinter.print(element, writer);
+		writer.append(" ");
+		typeReferencePrinter.print(element.getTypeReference(), writer);
+		arrayDimensionsPrinter.print(element.getArrayDimensionsBefore(), writer);
+		writer.append(" " + element.getName());
+		parametrizablePrinter.print(element, writer);
+		arrayDimensionsPrinter.print(element.getArrayDimensionsAfter(), writer);
+		exceptionThrowerPrinter.print(element, writer);
+		writer.append(" ");
+		statementPrinter.print(element.getStatement(), writer);
+		writer.append("\n");
+	}
+
+	private boolean shouldNotBePrinted(ClassMethod element) {
 		if (element.eContainer() instanceof Enumeration) {
 			var isStatic = false;
 			var isPublic = false;
@@ -62,25 +79,14 @@ public class ClassMethodPrinterImpl implements Printer<ClassMethod> {
 					var t = element.getParameters().get(0).getTypeReference().getTarget();
 					if (t instanceof tools.mdsd.jamopp.model.java.classifiers.Class cla
 							&& "java.lang.String".equals(cla.getQualifiedName())) {
-						return;
+						return true;
 					}
 				} else if ("values".equals(element.getName()) && element.getParameters().isEmpty()) {
-					return;
+					return true;
 				}
 			}
 		}
-		this.annotableAndModifiablePrinter.print(element, writer);
-		this.typeParametrizablePrinter.print(element, writer);
-		writer.append(" ");
-		this.typeReferencePrinter.print(element.getTypeReference(), writer);
-		this.arrayDimensionsPrinter.print(element.getArrayDimensionsBefore(), writer);
-		writer.append(" " + element.getName());
-		this.parametrizablePrinter.print(element, writer);
-		this.arrayDimensionsPrinter.print(element.getArrayDimensionsAfter(), writer);
-		this.exceptionThrowerPrinter.print(element, writer);
-		writer.append(" ");
-		this.statementPrinter.print(element.getStatement(), writer);
-		writer.append("\n");
+		return false;
 	}
 
 }
