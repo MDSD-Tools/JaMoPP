0c6aef5dc975c73853a0d160b733b338f3e781ef
unknown <yannickkraml@gmail.com>
2024-01-07 19:49:17 +0100

    Revert "Refactored ToNumberLiteralConverter"
    This reverts commit 0302cda785c08cef285887ff953d82ef262816cf.

--- bundles/tools.mdsd.jamopp.parser.jdt/src/tools/mdsd/jamopp/parser/jdt/implementation/converter/ToNumberLiteralConverter.java
+++ bundles/tools.mdsd.jamopp.parser.jdt/src/tools/mdsd/jamopp/parser/jdt/implementation/converter/ToNumberLiteralConverter.java
@@ -14,25 +14,19 @@
 package tools.mdsd.jamopp.parser.jdt.implementation.converter;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Predicate;
+
+import org.eclipse.jdt.core.dom.NumberLiteral;
+import tools.mdsd.jamopp.model.java.literals.LiteralsFactory;
 
 import javax.inject.Inject;
 
-import org.eclipse.jdt.core.dom.NumberLiteral;
-
-import tools.mdsd.jamopp.model.java.literals.Literal;
-import tools.mdsd.jamopp.model.java.literals.LiteralsFactory;
 import tools.mdsd.jamopp.parser.jdt.interfaces.converter.Converter;
 import tools.mdsd.jamopp.parser.jdt.interfaces.helper.UtilLayout;
 
-public class ToNumberLiteralConverter implements Converter<NumberLiteral, Literal> {
+public class ToNumberLiteralConverter implements Converter<NumberLiteral, tools.mdsd.jamopp.model.java.literals.Literal> {
 
 	private final LiteralsFactory lieteralsFactory;
 	private final UtilLayout layoutInformationConverter;
-	Map<Predicate<String>, Function<String, Literal>> mapping;
 
 	private static final String HEX_PREFIX = "0x";
 	private static final String BIN_PREFIX = "0b";
@@ -52,50 +46,63 @@ public class ToNumberLiteralConverter implements Converter<NumberLiteral, Litera
 	ToNumberLiteralConverter(UtilLayout layoutInformationConverter, LiteralsFactory lieteralsFactory) {
 		this.lieteralsFactory = lieteralsFactory;
 		this.layoutInformationConverter = layoutInformationConverter;
-		mapping = new HashMap<>();
-		mapping.put(string -> string.startsWith(BIN_PREFIX) && string.endsWith(LONG_SUFFIX),this::handleBinaryLong);
-		mapping.put(string -> string.startsWith(BIN_PREFIX), this::handleBinaryInteger);
-		mapping.put(string -> string.contains(HEX_EXPONENT) && string.endsWith(FLOAT_SUFFIX),this::handleHexFloat);
-		mapping.put(string -> string.contains(HEX_EXPONENT), this::handleHexDouble);
-		mapping.put(string -> string.startsWith(HEX_PREFIX) && string.endsWith(LONG_SUFFIX),this::handleHexLong);
-		mapping.put(string -> string.startsWith(HEX_PREFIX), this::handleHexInteger);
-		mapping.put(string -> string.endsWith(FLOAT_SUFFIX), this::handleDecimalFloat);
-		mapping.put(string -> string.contains(".") || string.contains(DECIMAL_EXPONENT) || string.endsWith(DOUBLE_SUFFIX),this::handleDecimalDouble);
-		mapping.put(string -> "0l".equals(string) || !string.startsWith(OCT_PREFIX) && string.endsWith(LONG_SUFFIX),this::handleDecimalLong);
-		mapping.put(string -> "0".equals(string) || !string.startsWith(OCT_PREFIX),this::handleDecimalInteger);
-		mapping.put(string -> string.endsWith(LONG_SUFFIX), this::handleOctalLong);
 	}
 
 	@Override
 	public tools.mdsd.jamopp.model.java.literals.Literal convert(NumberLiteral literal) {
+		tools.mdsd.jamopp.model.java.literals.Literal result = null;
 		String string = buildString(literal);
-		Literal result = mapping.getOrDefault(string, this::handleOctalInteger).apply(string);
-		layoutInformationConverter.convertToMinimalLayoutInformation(result, literal);
+		if (string.startsWith(BIN_PREFIX) && string.endsWith(LONG_SUFFIX)) {
+			result = handleBinaryLong(string);
+		} else if (string.startsWith(BIN_PREFIX)) {
+			result = handleBinaryInteger(string);
+		} else if (string.contains(HEX_EXPONENT) && string.endsWith(FLOAT_SUFFIX)) {
+			result = handleHexFloat(string);
+		} else if (string.contains(HEX_EXPONENT)) {
+			result = handleHexDouble(string);
+		} else if (string.startsWith(HEX_PREFIX) && string.endsWith(LONG_SUFFIX)) {
+			result = handleHexLong(string);
+		} else if (string.startsWith(HEX_PREFIX)) {
+			result = handleHexInteger(string);
+		} else if (string.endsWith(FLOAT_SUFFIX)) {
+			result = handleDecimalFloat(string);
+		} else if (string.contains(".") || string.contains(DECIMAL_EXPONENT) || string.endsWith(DOUBLE_SUFFIX)) {
+			result = handleDecimalDouble(string);
+		} else if ("0l".equals(string) || !string.startsWith(OCT_PREFIX) && string.endsWith(LONG_SUFFIX)) {
+			result = handleDecimalLong(string);
+		} else if ("0".equals(string) || !string.startsWith(OCT_PREFIX)) {
+			result = handleDecimalInteger(string);
+		} else if (string.endsWith(LONG_SUFFIX)) {
+			result = handleOctalLong(string);
+		} else {
+			result = handleOctalInteger(string);
+		}
+		this.layoutInformationConverter.convertToMinimalLayoutInformation(result, literal);
 		return result;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleOctalInteger(String string) {
-		tools.mdsd.jamopp.model.java.literals.OctalIntegerLiteral lit = lieteralsFactory.createOctalIntegerLiteral();
+		tools.mdsd.jamopp.model.java.literals.OctalIntegerLiteral lit = this.lieteralsFactory.createOctalIntegerLiteral();
 		lit.setOctalValue(new BigInteger(string.substring(OCT_PREFIX.length()), OCT_BASE));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleOctalLong(String string) {
-		tools.mdsd.jamopp.model.java.literals.OctalLongLiteral lit = lieteralsFactory.createOctalLongLiteral();
+		tools.mdsd.jamopp.model.java.literals.OctalLongLiteral lit = this.lieteralsFactory.createOctalLongLiteral();
 		lit.setOctalValue(new BigInteger(string.substring(OCT_PREFIX.length(), string.length() - LONG_SUFFIX.length()),
 				OCT_BASE));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleDecimalInteger(String string) {
-		tools.mdsd.jamopp.model.java.literals.DecimalIntegerLiteral lit = lieteralsFactory
+		tools.mdsd.jamopp.model.java.literals.DecimalIntegerLiteral lit = this.lieteralsFactory
 				.createDecimalIntegerLiteral();
 		lit.setDecimalValue(new BigInteger(string, DEC_BASE));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleDecimalLong(String string) {
-		tools.mdsd.jamopp.model.java.literals.DecimalLongLiteral lit = lieteralsFactory.createDecimalLongLiteral();
+		tools.mdsd.jamopp.model.java.literals.DecimalLongLiteral lit = this.lieteralsFactory.createDecimalLongLiteral();
 		lit.setDecimalValue(new BigInteger(string.substring(0, string.length() - LONG_SUFFIX.length()), DEC_BASE));
 		return lit;
 	}
@@ -104,25 +111,26 @@ public class ToNumberLiteralConverter implements Converter<NumberLiteral, Litera
 		if (string.endsWith(DOUBLE_SUFFIX)) {
 			string = string.substring(0, string.length() - DOUBLE_SUFFIX.length());
 		}
-		tools.mdsd.jamopp.model.java.literals.DecimalDoubleLiteral lit = lieteralsFactory.createDecimalDoubleLiteral();
+		tools.mdsd.jamopp.model.java.literals.DecimalDoubleLiteral lit = this.lieteralsFactory
+				.createDecimalDoubleLiteral();
 		lit.setDecimalValue(Double.parseDouble(string));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleDecimalFloat(String string) {
-		tools.mdsd.jamopp.model.java.literals.DecimalFloatLiteral lit = lieteralsFactory.createDecimalFloatLiteral();
+		tools.mdsd.jamopp.model.java.literals.DecimalFloatLiteral lit = this.lieteralsFactory.createDecimalFloatLiteral();
 		lit.setDecimalValue(Float.parseFloat(string.substring(0, string.length() - FLOAT_SUFFIX.length())));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleHexInteger(String string) {
-		tools.mdsd.jamopp.model.java.literals.HexIntegerLiteral lit = lieteralsFactory.createHexIntegerLiteral();
+		tools.mdsd.jamopp.model.java.literals.HexIntegerLiteral lit = this.lieteralsFactory.createHexIntegerLiteral();
 		lit.setHexValue(new BigInteger(string.substring(HEX_PREFIX.length()), HEX_BASE));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleHexLong(String string) {
-		tools.mdsd.jamopp.model.java.literals.HexLongLiteral lit = lieteralsFactory.createHexLongLiteral();
+		tools.mdsd.jamopp.model.java.literals.HexLongLiteral lit = this.lieteralsFactory.createHexLongLiteral();
 		lit.setHexValue(new BigInteger(string.substring(HEX_PREFIX.length(), string.length() - LONG_SUFFIX.length()),
 				HEX_BASE));
 		return lit;
@@ -132,25 +140,26 @@ public class ToNumberLiteralConverter implements Converter<NumberLiteral, Litera
 		if (string.endsWith(DOUBLE_SUFFIX)) {
 			string = string.substring(0, string.length() - DOUBLE_SUFFIX.length());
 		}
-		tools.mdsd.jamopp.model.java.literals.HexDoubleLiteral lit = lieteralsFactory.createHexDoubleLiteral();
+		tools.mdsd.jamopp.model.java.literals.HexDoubleLiteral lit = this.lieteralsFactory.createHexDoubleLiteral();
 		lit.setHexValue(Double.parseDouble(string));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleHexFloat(String string) {
-		tools.mdsd.jamopp.model.java.literals.HexFloatLiteral lit = lieteralsFactory.createHexFloatLiteral();
+		tools.mdsd.jamopp.model.java.literals.HexFloatLiteral lit = this.lieteralsFactory.createHexFloatLiteral();
 		lit.setHexValue(Float.parseFloat(string.substring(0, string.length() - FLOAT_SUFFIX.length())));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleBinaryInteger(String string) {
-		tools.mdsd.jamopp.model.java.literals.BinaryIntegerLiteral lit = lieteralsFactory.createBinaryIntegerLiteral();
+		tools.mdsd.jamopp.model.java.literals.BinaryIntegerLiteral lit = this.lieteralsFactory
+				.createBinaryIntegerLiteral();
 		lit.setBinaryValue(new BigInteger(string.substring(BIN_PREFIX.length()), BIN_BASE));
 		return lit;
 	}
 
 	private tools.mdsd.jamopp.model.java.literals.Literal handleBinaryLong(String string) {
-		tools.mdsd.jamopp.model.java.literals.BinaryLongLiteral lit = lieteralsFactory.createBinaryLongLiteral();
+		tools.mdsd.jamopp.model.java.literals.BinaryLongLiteral lit = this.lieteralsFactory.createBinaryLongLiteral();
 		lit.setBinaryValue(new BigInteger(string.substring(BIN_PREFIX.length(), string.length() - LONG_SUFFIX.length()),
 				BIN_BASE));
 		return lit;
