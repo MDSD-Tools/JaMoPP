dcf01dcadaf94d8e71fee1d876790cc6804f65d8
YKraml <82815144+YKraml@users.noreply.github.com>
2024-01-11 18:35:35 +0100

    Fixed warnings

--- bundles/tools.mdsd.jamopp.parser/src/tools/mdsd/jamopp/parser/jdt/implementation/converter/ToAdditiveOperatorConverter.java
+++ bundles/tools.mdsd.jamopp.parser/src/tools/mdsd/jamopp/parser/jdt/implementation/converter/ToAdditiveOperatorConverter.java
@@ -1,11 +1,11 @@
 package tools.mdsd.jamopp.parser.jdt.implementation.converter;
 
-import org.eclipse.jdt.core.dom.InfixExpression;
-import tools.mdsd.jamopp.model.java.operators.AdditiveOperator;
-import tools.mdsd.jamopp.model.java.operators.OperatorsFactory;
-
 import javax.inject.Inject;
 
+import org.eclipse.jdt.core.dom.InfixExpression;
+
+import tools.mdsd.jamopp.model.java.operators.AdditiveOperator;
+import tools.mdsd.jamopp.model.java.operators.OperatorsFactory;
 import tools.mdsd.jamopp.parser.jdt.interfaces.converter.Converter;
 
 public class ToAdditiveOperatorConverter implements Converter<InfixExpression.Operator, AdditiveOperator> {
@@ -13,19 +13,19 @@ public class ToAdditiveOperatorConverter implements Converter<InfixExpression.Op
 	private final OperatorsFactory operatorsFactory;
 
 	@Inject
-	ToAdditiveOperatorConverter(OperatorsFactory operatorsFactory) {
+	public ToAdditiveOperatorConverter(OperatorsFactory operatorsFactory) {
 		this.operatorsFactory = operatorsFactory;
 	}
 
 	@Override
-	public AdditiveOperator convert(InfixExpression.Operator op) {
-		if (op == InfixExpression.Operator.PLUS) {
-			return operatorsFactory.createAddition();
+	public AdditiveOperator convert(InfixExpression.Operator operator) {
+		AdditiveOperator result = null;
+		if (operator.equals(InfixExpression.Operator.PLUS)) {
+			result = operatorsFactory.createAddition();
+		} else if (operator.equals(InfixExpression.Operator.MINUS)) {
+			result = operatorsFactory.createSubtraction();
 		}
-		if (op == InfixExpression.Operator.MINUS) {
-			return operatorsFactory.createSubtraction();
-		}
-		return null;
+		return result;
 	}
 
 }
